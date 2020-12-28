dc50d270c9b1a075cb782e7fc64074b850e53cc2
retrymp3 <65720815+retrymp3@users.noreply.github.com>
2020-12-28 17:36:39 +0530

    Update bash_1.sh

--- easy/bash_1.sh
+++ easy/bash_1.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-echo "Give a number: "
-read number1
-echo "Give another number: "
-read number2
+read -p "Give a number: " number1
+read -p "Give a another number: " number2
 
-echo "`expr $number1 + $number2`"
-echo "`expr $number1 - $number2`"
-echo "`expr $number1 \* $number2`"
-echo "`expr $number1 / $number2`"
-echo "`expr $number1 % $number2`"
+expr $number1 + $number2 #Add
+expr $number1 - $number2 #Sub
+expr $number1 \* $number2 #Multiply
+expr $number1 / $number2 #Division
+expr $number1 % $number2 #Modulus
